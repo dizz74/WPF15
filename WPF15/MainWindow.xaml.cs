43043223ccb4231521d16532e8456655dbfbde75
dizz74 <dizz74@mail.ru>
2021-12-11 12:27:58 +0500

    Add project files.

---
+++ WPF15/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using System.Windows;
+
+namespace WPF15
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+      
+        }
+
+        private void open15wpf(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://www.2035designifmo.ru/mod/assign/view.php?id=1481");
+        }
+    }
+}
